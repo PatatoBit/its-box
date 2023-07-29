bb4e1b549cabd021f728f06d41dc27c9f1238a3d
Patato <pathonbusiness@gmail.com>
2023-07-29 16:05:00 +0700

    removed builds

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.4cf51bf1.js","_app/immutable/chunks/scheduler.1a5d8f86.js","_app/immutable/chunks/index.6cff9c76.js"];
-export const stylesheets = ["_app/immutable/assets/0.5ed6cf61.css"];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.f0120d9c.js","_app/immutable/chunks/scheduler.1a5d8f86.js","_app/immutable/chunks/index.6cff9c76.js","_app/immutable/chunks/index.89553c05.js"];
-export const stylesheets = ["_app/immutable/assets/2.4e3be46a.css"];
-export const fonts = [];
